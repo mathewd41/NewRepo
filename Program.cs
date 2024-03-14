36cf93086bcd87d81ebd91747f1ae9209f2a01af
mathewd41 <mathewd4@mymacewan.ca>
2024-03-13 18:39:09 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿
+using C_hash_Ass1_Dhanya_Mathew;
+
+public class Program
+{
+
+
+    static Student CreateStudent(string id, StudentType type, string firstName, string lastName)
+    {
+
+        id ??= "0";
+        var student = new Student();
+        student.id = Int32.Parse(id);
+        student.type = type;
+        student.firstName = firstName ?? "Unknown";
+        student.lastName = lastName ?? "";
+        return student;
+    }
+
+    static void OutputStudent(Student student)
+    {
+        Console.WriteLine($"Student: {student.id:D3} {student.firstName} {student.lastName} ({student.type}) ");
+    }
+
+    static void Main(string[] args)
+    {
+        var student1 = CreateStudent("1", StudentType.FullTime, "John", "Anderson");
+        var student2 = CreateStudent("2", StudentType.FullTime, "Karen", "Smith");
+        var student3 = CreateStudent("3", StudentType.PartTime, "Kevin", "Peters");
+        Console.WriteLine("Assignment One");
+        OutputStudent(student1);
+        OutputStudent(student2);
+        OutputStudent(student3);
+
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_hash_Ass1_Dhanya_Mathew
+{
+   internal class Student
+    {
+        public int id ;
+        public StudentType type;
+        public string firstName;
+        public string lastName;
+
+    }
+
+
+  
+}
